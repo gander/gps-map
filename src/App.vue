83e6bd18c7b7ab910a882e3404e3ff56d437b32f
Adam Gąsowski <gander@users.noreply.github.com>
2024-01-24 15:39:40 +0100

    install and configure OpenLayers

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  APP
+  <ol-map
+      id="map"
+      :loadTilesWhileAnimating="true"
+      :loadTilesWhileInteracting="true"
+  >
+    <ol-view
+        ref="view"
+        :center="center"
+        :rotation="rotation"
+        :zoom="zoom"
+        :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm/>
+    </ol-tile-layer>
+  </ol-map>
 </template>
 
-<style scoped>
+<script setup>
+import {ref} from "vue";
+import {fromLonLat} from "ol/proj";
+
+const center = ref(fromLonLat([21.0122, 52.2297]));
+const projection = ref("EPSG:3857");
+const zoom = ref(7);
+const rotation = ref(0);
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  height: 100%;
+}
+
+
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from '@/App.vue'
+import {createApp} from 'vue';
+import App from '@/App.vue';
+import OpenLayersMap from 'vue3-openlayers';
 
-createApp(App).mount('#app')
+createApp(App).use(OpenLayersMap, {debug: true}).mount('#app');
