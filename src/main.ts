f0af52529390a0969ad8648b572efbea1e183dc4
Adam Gąsowski <gander@users.noreply.github.com>
2024-01-24 17:52:36 +0100

    config debug on development only

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import {createApp} from 'vue';
 import App from '@/App.vue';
 import OpenLayersMap from 'vue3-openlayers';
 
-createApp(App).use(OpenLayersMap, {debug: true}).mount('#app');
+createApp(App).use(OpenLayersMap, {debug: import.meta.env.DEV}).mount('#app');
