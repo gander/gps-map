a3ee6a6e5ae799b0f33818b4da79d2df75b69303
Adam Gąsowski <gander@users.noreply.github.com>
2024-01-25 15:01:55 +0100

    feat: releasing versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from 'node:url';
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(() => {
+    return {
+        plugins: [
+            vue(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+        define: {
+            'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version),
+        },
+    };
+});
